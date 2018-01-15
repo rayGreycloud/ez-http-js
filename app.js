9f2590148c9a3671652465e3263a421055532cd6
rayGreycloud <rayGreycloud@gmail.com>
2018-01-15 06:47:28 -0800

    Add get request for single post

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const http = new ezHTTP();
 
-// GET data from API  
-http.get('https://jsonplaceholder.typicode.com/posts', function (err, response) {
+// GET request for posts to testing API  
+// http.get('https://jsonplaceholder.typicode.com/posts', function (err, response) {
+//   if (err) {
+//     console.log(err);  
+//   } else {
+//     console.log(response);    
+//   }
+// });
+
+// GET request for single post to testing API  
+http.get('https://jsonplaceholder.typicode.com/posts/1', function (err, response) {
   if (err) {
     console.log(err);  
   } else {
     console.log(response);    
   }
-
 });
