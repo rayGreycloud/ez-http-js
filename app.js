cc8a323c6d0bb243fab554a0e8a0886fc5975bfe
rayGreycloud <rayGreycloud@gmail.com>
2018-01-15 07:05:03 -0800

    Add post request with dummy data

--- app.js
+++ app.js
@@ -9,11 +9,31 @@ const http = new ezHTTP();
 //   }
 // });
 
-// GET request for single post to testing API  
-http.get('https://jsonplaceholder.typicode.com/posts/1', function (err, response) {
-  if (err) {
-    console.log(err);  
-  } else {
-    console.log(response);    
+// // GET request for single post to testing API  
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function (err, response) {
+//   if (err) {
+//     console.log(err);  
+//   } else {
+//     console.log(response);    
+//   }
+// });
+
+// POST request to add post to testing API  
+const data = {
+  userId: 500,
+//  id: 200,
+  title: "A Made-up Title",
+  body: "Yada Yada Yada Bing Bing Bing\nYada Yada Yada Bing Bing Bing"
+};
+
+http.post(
+  'https://jsonplaceholder.typicode.com/posts',
+  data,
+  function (err, response) {
+    if (err) {
+      console.log(err);  
+    } else {
+      console.log(response);    
+    }
   }
-});
+);
